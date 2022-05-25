86792ab9aded40ef44af88b6eec408192c2ce774
Diego Bernardi <diego-bs@hotmail.com>
2022-05-25 16:39:01 -0300

    sinil authentication app

---
+++ sinil_auth/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SinilAuthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sinil_auth'
---
+++ sinil_auth/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from django.contrib.auth.decorators import login_required 
+
+#@login_required
+def perfil(request): 
+
+    return render(request, "sinil_auth/perfil.html")
