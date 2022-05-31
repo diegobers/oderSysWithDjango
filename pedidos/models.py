09ab6d45e0460f36cc39fdbf92c2f2357c1554be
Diego Bernardi <diego-bs@hotmail.com>
2022-05-31 04:36:22 -0300

    remove base template

--- pedidos/models.py
+++ pedidos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Perfil(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="perfil")
+    bio = models.TextField()
+
+    def __str__(self):
+        return f"{self.__class__.__name__} object for {self.user}"
